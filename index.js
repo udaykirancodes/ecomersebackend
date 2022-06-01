e7746dbcf4f3b47ae7d3fcce68a49419be4762b1
Vs-karma <81770024+Vs-karma@users.noreply.github.com>
2022-06-01 10:55:31 +0530

    checking

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ mongoose
 .then(()=>{
     console.log('Db Connected');
     app.listen(config.port,()=>{
-        console.log('Server Started'); 
+        console.log('Server Started'); //server started
     }) 
 })
 .catch((err)=>{
