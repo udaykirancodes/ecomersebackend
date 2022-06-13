4e5f6d8cf3322bce6d461b7affd484f9070be68b
Udaykiran <officialudaykiran@gmail.com>
2022-06-13 21:59:21 +0530

    updated

--- routes/Product/Find.js
+++ routes/Product/Find.js
@@ -7,7 +7,7 @@ const Product = require('../../models/Product');
 router.get('/all',async (req,res)=>{
     try {
         let products = await Product.find();
-        res.status(200).json({success:true,data:products}); 
+        res.status(200).json({success:true,products:products}); 
     } catch (error) {
         console.log(error.message);
         res.status(500).json({success:false,msg:'Internal Server Error'});
