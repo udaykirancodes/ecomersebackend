bf3162cd3137e68e7d6e4984dcc3b1044ecad1f8
Udaykiran <officialudaykiran@gmail.com>
2022-06-20 17:16:23 +0530

    Buying Cancel Routes

--- routes/index.js
+++ routes/index.js
@@ -1,40 +1,40 @@
-const express = require('express'); 
-const router = express.Router(); 
+const express = require('express');
+const router = express.Router();
 
 // importing routers 
-const Auth = require('./auth/index');  
-const Products = require('./Product/index'); 
-const Blogs = require('./Blogs/index'); 
-const Subscribers = require('./Subscribers/index'); 
-const Emails = require('./Emails/index'); 
-const WishList = require('./WishList/index'); 
-const Sell = require('./Sell/index'); 
+const Auth = require('./auth/index');
+const Products = require('./Product/index');
+const Blogs = require('./Blogs/index');
+const Subscribers = require('./Subscribers/index');
+const Emails = require('./Emails/index');
+const WishList = require('./WishList/index');
+const Sell = require('./Sell/index');
 
 //authentication routes 
-router.use('/auth',Auth); 
+router.use('/auth', Auth);
 
 //product routes 
-router.use('/products',Products);
+router.use('/products', Products);
 
 // blog product routers 
-router.use('/blogs',Blogs)
+router.use('/blogs', Blogs)
 
 // subscriber routes 
-router.use('/subscribers',Subscribers); 
+router.use('/subscribers', Subscribers);
 
 // Email - Marketing Routes 
-router.use('/emails',Emails);  
+router.use('/emails', Emails);
 
 // wishlist routes 
-router.use('/wishlist',WishList)
+router.use('/wishlist', WishList)
 
 // sell route (customer selling )
-router.use('/sell',Sell); 
+router.use('/sell', Sell);
 
 
 // base root 
-router.get('/',(req,res)=>{
+router.get('/', (req, res) => {
     res.send('Hello👋')
 })
 
-module.exports = router ; 
+module.exports = router; 
